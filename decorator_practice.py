86e1357c180d43c0410ac9b3579e9d40aa98f127
Prudhvi Kumar Sunkara <s.prudhvi22@gmail.com>
2020-08-03 16:42:59 -0700

    decorator

---
+++ decorator_practice.py
@@ -0,0 +1,18 @@
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print("time taken in secs: ", end-start)
+    return wrapper
+
+
+@timer
+def sum(a,b):
+    print(a+b)
+
+
+sum(5,6785644)
